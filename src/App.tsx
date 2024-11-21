149c450a0406e58c3138da8a011512fcff31e3c0
Sanjeev Rai <sunzeevraee@gmail.com>
2024-11-21 12:01:35 +0545

    nothing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./Index.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
   return <>This is my first React in Typescript</>;
 }
 
